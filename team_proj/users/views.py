94828af65d82fe867f291cdbb1029334eaec3043
dimaklymenko <dimaliar228@gmail.com>
2025-03-13 22:24:04 +0200

    Reset password with email

--- team_proj/users/views.py
+++ team_proj/users/views.py
@@ -1,28 +1,59 @@
-from django.shortcuts import render, redirect
-from django.views import View
+from .forms import RegisterForm, LoginForm
+
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
-from .forms import RegisterForm
 
 
-class RegisterView(View):
-    template_name = 'users/signup.html'
-    form_class = RegisterForm
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
 
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect('contacts:home')
-        return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request):
-        return render(request, self.template_name, {'form': self.form_class})
 
-    def post(self, request):
-        form = self.form_class(request.POST)
+def signupuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='contacts:home')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data['username']
-            messages.success(request, f"Congratulations to {username}. Your account has been successfully created.")
-            return redirect('users:login')
-        return render(request, self.template_name, {'form': form})
+            return redirect(to='contacts:home')
+        else:
+            return render(request, 'users/signup.html', context={"form": form})
+
+    return render(request, 'users/signup.html', context={"form": RegisterForm()})
+
+
+
+def loginuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='contacts:home')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
+            return redirect(to='users:login')
+
+        login(request, user)
+        return redirect(to='contacts:home')
+
+    return render(request, 'users/login.html', context={"form": LoginForm()})
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='contacts:home')
+
+
