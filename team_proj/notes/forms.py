fe4a55df84851f50fa38b863b5b8e54f96670d0d
Alex2025 <tesler2000@gmail.com>
2025-03-19 15:19:20 +0200

    tag filters and sorting

--- team_proj/notes/forms.py
+++ team_proj/notes/forms.py
@@ -1,16 +1,26 @@
-from django.forms import ModelForm, CharField, TextInput,CheckboxSelectMultiple,ModelMultipleChoiceField 
-from .models import Note,Tag
+# forms.py (in your notes app)
+from django import forms
+from .models import Note, Tag
 
-
-
-class NoteForm(ModelForm):
-    tags = ModelMultipleChoiceField(queryset=Tag.objects.all(), required=False, widget=CheckboxSelectMultiple())
+class NoteForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+    new_tags = forms.CharField(
+        max_length=200,
+        required=False,
+        help_text="Enter new tags, separated by commas.",
+    )
 
     class Meta:
         model = Note
-        fields = ['name', 'description'] # tags is removed from here
+        fields = ['name', 'description', 'tags', 'new_tags']
 
-class TagForm(ModelForm):
-    class Meta:
-        model = Tag
-        fields = ['name']
+    def clean_new_tags(self):
+        new_tags = self.cleaned_data.get('new_tags')
+        if new_tags:
+            return [tag.strip() for tag in new_tags.split(',')]
+        return []
+
